982cdfedc28ba480e3aefcfc99c41808ec14f5b3
Piotr Zapała <piotrekapala25@gmail.com>
2023-01-14 05:50:44 +0100

    imp: app class

--- client/src/main/java/com/client/App.java
+++ client/src/main/java/com/client/App.java
@@ -1,5 +1,17 @@
 package com.client;
 
+/**
+ * Class which invoke the program
+ */
 public class App {
 
+<<<<<<< Updated upstream
+=======
+    /**
+     * Method to invokes all program
+     */
+    public static void main(String[] args) {
+        new Window().createWindow();
+    }
+>>>>>>> Stashed changes
 }
