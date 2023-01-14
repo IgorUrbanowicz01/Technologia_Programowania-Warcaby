27576e8a692fc8f5e497b9b097d343428113fac0
Piotr Zapała <piotrekapala25@gmail.com>
2023-01-14 05:52:32 +0100

    imp: window class

--- client/src/main/java/com/client/Window.java
+++ client/src/main/java/com/client/Window.java
@@ -1,5 +1,46 @@
 package com.client;
 
-public class Window {
-    
+import com.client.helpers.Routes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+import javafx.stage.Stage;
+
+public class Window extends Application {
+
+    public void createWindow() {
+        launch();
+    }
+
+    /**
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Stage stage = new Stage();
+        ClientCore.getInstance().programStage = primaryStage;
+        // FXML loader
+        Parent sceneRoot = FXMLLoader.load(Routes.viewsRoute("StartView.fxml"));
+        // Basic scene
+        Scene scene = new Scene(sceneRoot, 800, 600);
+        scene.getStylesheets().add(Routes.styleRoute("app.css"));
+        // Icone
+        Image icon = new Image(Routes.imageRoute("LogoIcon.png"));
+
+        // Stage settings
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("WarcabyGame");
+        primaryStage.setScene(scene);
+        primaryStage.setOnCloseRequest(windowEvent -> ClientCore.getInstance().close());
+        primaryStage.show();
+        primaryStage.setResizable(false);
+        primaryStage.centerOnScreen();
+
+    }
+
 }
